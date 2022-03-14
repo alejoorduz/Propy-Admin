05d5fe391bfc54190fc7631a590867036443f9ad
Alejandro Orduz <alejorduz@gmail.com>
2022-03-14 18:26:57 -0500

    modificado para recibir planes, basico, estandar, pro

--- src/app/administraciones/adding-proyect/adding-proyect.page.ts
+++ src/app/administraciones/adding-proyect/adding-proyect.page.ts
@@ -30,13 +30,15 @@ export class AddingProyectPage implements OnInit {
   sabado: boolean
   domingo: boolean
 
+  //Plan elegido por usuario
+  plan: string;
   //servicios disponibles por la app
-  reservas: boolean
-  pagos: boolean = false
-  comunicados: boolean
-  documentos: boolean = false
-  monitoreo:boolean = false
-  aircall: boolean
+  // reservas: boolean
+  // pagos: boolean = false
+  // comunicados: boolean
+  // documentos: boolean = false
+  // monitoreo:boolean = false
+  // aircall: boolean
   //Variables del picker de horas admitidas por reserva, hora inicial y final
   hora_inicial: number
   hora_final: number
@@ -49,7 +51,7 @@ export class AddingProyectPage implements OnInit {
   servicio;
 
 //,public alertController: AlertController,public formModule: FormsModule
-  constructor(private alertCtrl: AlertController,public navCtrl: NavController,private storage: Storage,private modalCtrl: ModalController,private firestoreService: FirestoreService) { }
+  constructor(public alertController: AlertController, private alertCtrl: AlertController,public navCtrl: NavController,private storage: Storage,private modalCtrl: ModalController,private firestoreService: FirestoreService) { }
 
   
   ngOnInit(): void {
@@ -58,6 +60,73 @@ export class AddingProyectPage implements OnInit {
 
   async ionViewWillEnter() {
     await this.storage.create();
+  }
+
+  checkbasic(){
+    console.log("Plan basico")
+    console.log($("#basico").prop("checked"))
+    if ($("#basico").prop("checked") === false) {
+      $("#estandar").prop("checked",false)
+      $("#pro").prop("checked",false)
+      this.plan = "basico"
+    }
+  }
+  checkstandart(){
+    console.log("Plan estandar")
+    console.log($("#estandar").prop("checked"))
+    if ($("#estandar").prop("checked") === false) {
+      $("#basico").prop("checked",false)
+      $("#pro").prop("checked",false)
+      this.plan = "estandar"
+    }
+  }
+  checkpro(){
+    console.log("Plan pro")
+    console.log($("#pro").prop("checked"))
+    if ($("#pro").prop("checked") === false) {
+      $("#estandar").prop("checked",false)
+      $("#basico").prop("checked",false)
+      this.plan = "pro"
+    }
+  }
+
+  async presentBasic() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: '¡Plan Básico!',
+      subHeader: 'Incluye los siguientes servicios',
+      message: 'Reservas, AirCall, Comunicados, Mascotas, Aviso de trasteo, Directorio, Autorizaciones, Preguntas, Emergencia Ascensor, Eventos',
+      buttons: ['OK']
+    });
+    await alert.present();
+    const { role } = await alert.onDidDismiss();
+    console.log('onDidDismiss resolved with role', role);
+  }
+
+  async presentStandart() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Plan Estandar!',
+      subHeader: 'Incluye estos servicios',
+      message: 'Plan Básico + Documentos, Clasificados, Encuestas, Controles de Acceso',
+      buttons: ['OK']
+    });
+    await alert.present();
+    const { role } = await alert.onDidDismiss();
+    console.log('onDidDismiss resolved with role', role);
+  }
+
+  async presentPro() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Plan Pro!',
+      subHeader: 'Incluye estos servicios',
+      message: 'Plan Estándar + Pagos, Monitoreo, Finanzas, Beneficios, Seguridad, Citofonia',
+      buttons: ['OK']
+    });
+    await alert.present();
+    const { role } = await alert.onDidDismiss();
+    console.log('onDidDismiss resolved with role', role);
   }
 
  //funcion que guarda los dias de la semana habilitados en un array
@@ -90,17 +159,16 @@ export class AddingProyectPage implements OnInit {
           // this.lista_periodo = $("#lista_periodo").val()
           this.hora_inicial = $("#hora_inicial").val()
           this.hora_final = $("#hora_final").val()
-
-          this.reservas = $("#reservas").prop("checked")
+          // this.reservas = $("#reservas").prop("checked")
           //this.pagos = $("#pagos").prop("checked")
-          this.comunicados = $("#comunicados").prop("checked")
+          // this.comunicados = $("#comunicados").prop("checked")
           // this.documentos = $("#documentos").prop("checked")
           //this.monitoreo = $("#monitoreo").prop("checked")
-          this.aircall = $("#aircall").prop("checked")
+          // this.aircall = $("#aircall").prop("checked")
 
           //  this.horas_reserva = $("#horas_reserva").val()
           //this.storage.set('servicio 1', this.martes)
-          console.log("los datos: " ,this.aircall, this.proyecto, this.hora_inicial , this.hora_final, this.reservas, this.pagos, this.lunes, this.domingo)
+          // console.log("los datos: " ,this.aircall, this.proyecto, this.hora_inicial , this.hora_final, this.reservas, this.pagos, this.lunes, this.domingo)
           this.upload_reservation_data()
           // let horaenvio = new Date(Date.now());
           // var ti = moment(horaenvio).format('h:mm a');
@@ -126,6 +194,7 @@ export class AddingProyectPage implements OnInit {
   }
 
   upload_reservation_data(){
+    console.log("plan elegido:", this.plan)
     this.firestoreService.insertardos("Admins",this.uid,"proyectos", this.proyecto, {"horainicial": this.hora_inicial} )
     this.firestoreService.updatedos("Admins",this.uid,"proyectos", this.proyecto,  {"horafinal": this.hora_final} )
     this.firestoreService.updatedos("Admins",this.uid,"proyectos", this.proyecto,  {"lunes": this.lunes} )
@@ -135,34 +204,112 @@ export class AddingProyectPage implements OnInit {
     this.firestoreService.updatedos("Admins",this.uid,"proyectos", this.proyecto,  {"viernes": this.viernes}  )
     this.firestoreService.updatedos("Admins",this.uid,"proyectos", this.proyecto,   {"sabado": this.sabado} )
     this.firestoreService.updatedos("Admins",this.uid,"proyectos", this.proyecto,   {"domingo": this.domingo} )
-    this.firestoreService.updatedos("Admins",this.uid,"proyectos", this.proyecto,   {"reservas": this.reservas})
-    this.firestoreService.updatedos("Admins",this.uid,"proyectos", this.proyecto,  {"pagos": this.pagos}  )
-    this.firestoreService.updatedos("Admins",this.uid,"proyectos", this.proyecto,   {"comunicados": this.comunicados} )
-    this.firestoreService.updatedos("Admins",this.uid,"proyectos", this.proyecto,   {"documentos": this.documentos} )
-    this.firestoreService.updatedos("Admins",this.uid,"proyectos", this.proyecto,   {"monitoreo": this.monitoreo} )
-    this.firestoreService.updatedos("Admins",this.uid,"proyectos", this.proyecto,   {"aircall": this.aircall} )
-  
- 
-    this.firestoreService.insertar("Proyectos", this.proyecto, {"horainicial": this.hora_inicial} )
-    this.firestoreService.update("Proyectos", this.proyecto, {"horafinal": this.hora_final} )
-    this.firestoreService.update("Proyectos", this.proyecto, {"lunes": this.lunes} )
-    this.firestoreService.update("Proyectos", this.proyecto,  {"martes": this.martes} )
-    this.firestoreService.update("Proyectos", this.proyecto,  {"miercoles": this.miercoles})
-    this.firestoreService.update("Proyectos", this.proyecto,  {"jueves": this.jueves})
-    this.firestoreService.update("Proyectos", this.proyecto,  {"viernes": this.viernes}  )
-    this.firestoreService.update("Proyectos", this.proyecto,   {"sabado": this.sabado} )
-    this.firestoreService.update("Proyectos", this.proyecto,  {"domingo": this.domingo} )
-    this.firestoreService.update("Proyectos", this.proyecto,  {"reservas": this.reservas})
-    this.firestoreService.update("Proyectos", this.proyecto,  {"pagos": this.pagos}  )
-    this.firestoreService.update("Proyectos", this.proyecto,   {"comunicados": this.comunicados} )
-    this.firestoreService.update("Proyectos", this.proyecto,   {"documentos": this.documentos} )
-    this.firestoreService.update("Proyectos", this.proyecto,   {"monitoreo": this.monitoreo} )
-    this.firestoreService.update("Proyectos", this.proyecto,   {"aircall": this.aircall} )
-    this.firestoreService.update("Proyectos", this.proyecto,   {"key": this.password} )
-    //this.firestoreService.update("Proyectos", this.proyecto,   {"periodo": this.lista_periodo} )
+    
+    if(this.plan === "basico"){
+      console.log("si, basico")
+      this.firestoreService.insertar("Proyectos", this.proyecto, {"horainicial": this.hora_inicial} )
+      this.firestoreService.update("Proyectos", this.proyecto, {"horafinal": this.hora_final} )
+      this.firestoreService.update("Proyectos", this.proyecto, {"lunes": this.lunes} )
+      this.firestoreService.update("Proyectos", this.proyecto,  {"martes": this.martes} )
+      this.firestoreService.update("Proyectos", this.proyecto,  {"miercoles": this.miercoles})
+      this.firestoreService.update("Proyectos", this.proyecto,  {"jueves": this.jueves})
+      this.firestoreService.update("Proyectos", this.proyecto,  {"viernes": this.viernes}  )
+      this.firestoreService.update("Proyectos", this.proyecto,   {"sabado": this.sabado} )
+      this.firestoreService.update("Proyectos", this.proyecto,  {"domingo": this.domingo} )
+      this.firestoreService.update("Proyectos", this.proyecto,   {"key": this.password} )
+      this.firestoreService.update("Proyectos", this.proyecto,   {"reservas": true})
+      this.firestoreService.update("Proyectos", this.proyecto,  {"aircall": true}  )
+      this.firestoreService.update("Proyectos", this.proyecto,  {"comunicados": true} )
+      this.firestoreService.update("Proyectos", this.proyecto,  {"mascotas": true} )
+      this.firestoreService.update("Proyectos", this.proyecto,   {"trasteo": true} )
+      this.firestoreService.update("Proyectos", this.proyecto,  {"directorio": true} )
+      this.firestoreService.update("Proyectos", this.proyecto,  {"autorizaciones": true})
+      this.firestoreService.update("Proyectos", this.proyecto, {"preguntas": true}  )
+      this.firestoreService.update("Proyectos", this.proyecto,   {"emergencias": true} )
+      this.firestoreService.update("Proyectos", this.proyecto,   {"eventos": true} )
+      this.firestoreService.update("Proyectos", this.proyecto,  {"documentos": false} )
+      this.firestoreService.update("Proyectos", this.proyecto,  {"clasificados": false} )
+      this.firestoreService.update("Proyectos", this.proyecto,  {"encuestas": false} )
+      this.firestoreService.update("Proyectos", this.proyecto,  {"acceso": false} )
+      this.firestoreService.update("Proyectos", this.proyecto,   {"pagos": false} )
+      this.firestoreService.update("Proyectos", this.proyecto,   {"monitoreo": false} )
+      this.firestoreService.update("Proyectos", this.proyecto,  {"finanzas": false} )
+      this.firestoreService.update("Proyectos", this.proyecto,  {"beneficios": false} )
+      this.firestoreService.update("Proyectos", this.proyecto,   {"seguridad": false} )
+      this.firestoreService.update("Proyectos", this.proyecto,  {"citofonia": false} )
+    }
+
+    if(this.plan === "estandar"){
+      console.log("si, estandar")
+      this.firestoreService.insertar("Proyectos", this.proyecto, {"horainicial": this.hora_inicial} )
+      this.firestoreService.update("Proyectos", this.proyecto, {"horafinal": this.hora_final} )
+      this.firestoreService.update("Proyectos", this.proyecto, {"lunes": this.lunes} )
+      this.firestoreService.update("Proyectos", this.proyecto,  {"martes": this.martes} )
+      this.firestoreService.update("Proyectos", this.proyecto,  {"miercoles": this.miercoles})
+      this.firestoreService.update("Proyectos", this.proyecto,  {"jueves": this.jueves})
+      this.firestoreService.update("Proyectos", this.proyecto,  {"viernes": this.viernes}  )
+      this.firestoreService.update("Proyectos", this.proyecto,   {"sabado": this.sabado} )
+      this.firestoreService.update("Proyectos", this.proyecto,  {"domingo": this.domingo} )
+      this.firestoreService.update("Proyectos", this.proyecto,   {"key": this.password} )
+      this.firestoreService.update("Proyectos", this.proyecto,   {"reservas": true})
+      this.firestoreService.update("Proyectos", this.proyecto,  {"aircall": true}  )
+      this.firestoreService.update("Proyectos", this.proyecto,  {"comunicados": true} )
+      this.firestoreService.update("Proyectos", this.proyecto,  {"mascotas": true} )
+      this.firestoreService.update("Proyectos", this.proyecto,   {"trasteo": true} )
+      this.firestoreService.update("Proyectos", this.proyecto,  {"directorio": true} )
+      this.firestoreService.update("Proyectos", this.proyecto,  {"autorizaciones": true})
+      this.firestoreService.update("Proyectos", this.proyecto, {"preguntas": true}  )
+      this.firestoreService.update("Proyectos", this.proyecto,   {"emergencias": true} )
+      this.firestoreService.update("Proyectos", this.proyecto,   {"eventos": true} )
+      this.firestoreService.update("Proyectos", this.proyecto,  {"documentos": true} )
+      this.firestoreService.update("Proyectos", this.proyecto,  {"clasificados": true} )
+      this.firestoreService.update("Proyectos", this.proyecto,  {"encuestas": true} )
+      this.firestoreService.update("Proyectos", this.proyecto,  {"acceso": true} )
+      this.firestoreService.update("Proyectos", this.proyecto,   {"pagos": false} )
+      this.firestoreService.update("Proyectos", this.proyecto,   {"monitoreo": false} )
+      this.firestoreService.update("Proyectos", this.proyecto,  {"finanzas": false} )
+      this.firestoreService.update("Proyectos", this.proyecto,  {"beneficios": false} )
+      this.firestoreService.update("Proyectos", this.proyecto,   {"seguridad": false} )
+      this.firestoreService.update("Proyectos", this.proyecto,  {"citofonia": false} )
+      
+    }
+
+    if(this.plan === "pro"){
+      console.log("si, pro")
+      this.firestoreService.insertar("Proyectos", this.proyecto, {"horainicial": this.hora_inicial} )
+      this.firestoreService.update("Proyectos", this.proyecto, {"horafinal": this.hora_final} )
+      this.firestoreService.update("Proyectos", this.proyecto, {"lunes": this.lunes} )
+      this.firestoreService.update("Proyectos", this.proyecto,  {"martes": this.martes} )
+      this.firestoreService.update("Proyectos", this.proyecto,  {"miercoles": this.miercoles})
+      this.firestoreService.update("Proyectos", this.proyecto,  {"jueves": this.jueves})
+      this.firestoreService.update("Proyectos", this.proyecto,  {"viernes": this.viernes}  )
+      this.firestoreService.update("Proyectos", this.proyecto,   {"sabado": this.sabado} )
+      this.firestoreService.update("Proyectos", this.proyecto,  {"domingo": this.domingo} )
+      this.firestoreService.update("Proyectos", this.proyecto,   {"key": this.password} )
+      this.firestoreService.update("Proyectos", this.proyecto,   {"reservas": true})
+      this.firestoreService.update("Proyectos", this.proyecto,  {"aircall": true}  )
+      this.firestoreService.update("Proyectos", this.proyecto,  {"comunicados": true} )
+      this.firestoreService.update("Proyectos", this.proyecto,  {"mascotas": true} )
+      this.firestoreService.update("Proyectos", this.proyecto,   {"trasteo": true} )
+      this.firestoreService.update("Proyectos", this.proyecto,  {"directorio": true} )
+      this.firestoreService.update("Proyectos", this.proyecto,  {"autorizaciones": true})
+      this.firestoreService.update("Proyectos", this.proyecto, {"preguntas": true}  )
+      this.firestoreService.update("Proyectos", this.proyecto,   {"emergencias": true} )
+      this.firestoreService.update("Proyectos", this.proyecto,   {"eventos": true} )
+      this.firestoreService.update("Proyectos", this.proyecto,  {"documentos": true} )
+      this.firestoreService.update("Proyectos", this.proyecto,  {"clasificados": true} )
+      this.firestoreService.update("Proyectos", this.proyecto,  {"encuestas": true} )
+      this.firestoreService.update("Proyectos", this.proyecto,  {"acceso": true} )
+      this.firestoreService.update("Proyectos", this.proyecto,   {"pagos": true} )
+      this.firestoreService.update("Proyectos", this.proyecto,   {"monitoreo": true} )
+      this.firestoreService.update("Proyectos", this.proyecto,  {"finanzas": true} )
+      this.firestoreService.update("Proyectos", this.proyecto,  {"beneficios": true} )
+      this.firestoreService.update("Proyectos", this.proyecto,   {"seguridad": true} )
+      this.firestoreService.update("Proyectos", this.proyecto,  {"citofonia": true} )
+    }
     setTimeout(()=>{
     this.modalCtrl.dismiss()
-    },300)
+    },500)
     // .then(() => {
     //   console.log('Datos subidos correctamente!');
     //   //this.tareaEditando= {} as Tarea;
